e78b42d43a119b15286ebf24682b6d47d03b6656
Fiona Steiner <116308454+Fiona-Steiner@users.noreply.github.com>
2024-03-14 17:08:07 +0100

    Update accueil.py

--- Application/accueil.py
+++ Application/accueil.py
@@ -1,12 +1,36 @@
 import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
 
-# Titre de la page d'accueil
-st.title("Bienvenue sur notre Chatbot")
+
+
+st.set_page_config(page_title="accueil", initial_sidebar_state="collapsed")
+
+st.title("Bienvenue sur notre app !")
 
 # Description de l'application
 st.write("""
-         Cette application vous permet d'interagir avec notre chatbot alimenté par Mixtral LLM.
-         Posez-lui des questions et il vous fournira des réponses en fonction de votre requête.
-         Pour commencer à discuter avec le chatbot, cliquez sur le bouton ci-dessous.
-         """)
+        Préparez vos oreilles et ajustez vos enceintes, car notre projet va révolutionner votre playlist comme jamais auparavant! Oubliez le casse-tête de chercher la prochaine perle rare. 
 
+Alors, êtes-vous prêts à laisser un algorithme décider de vos futurs coups de cœur musicaux ? Attention, risque élevé de devenir accro! 😊 
+            """)
+
+# Boutons pour naviguer vers d'autres pages
+st.write("🤖 Besoin d'un ami virtuel ? Cliquez ici ! ")
+if st.button("Chatbot"):
+    switch_page("chatbot")
+
+st.write("♫ Envie de swinguer ? Téléchargez vos rythmes préférés !")
+if st.button("Shazam"):
+    switch_page("shazam")
+
+
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
